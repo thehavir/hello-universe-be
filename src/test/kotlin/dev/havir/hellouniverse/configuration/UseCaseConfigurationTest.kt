607c92704fe7a24067511e57cccd7141f5579c75
havir <havir.valizadeh@klar.mx>
2026-03-15 16:26:39 +0100

    Implement FetchTodayApodUseCase

---
+++ src/test/kotlin/dev/havir/hellouniverse/configuration/UseCaseConfigurationTest.kt
@@ -0,0 +1,30 @@
+package dev.havir.hellouniverse.configuration
+
+import dev.havir.hellouniverse.domain.ApodGateway
+import dev.havir.hellouniverse.domain.ApodRepository
+import io.mockk.mockk
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.assertNotNull
+import kotlin.test.Test
+
+class UseCaseConfigurationTest {
+    lateinit var apodGateway: ApodGateway
+    lateinit var apodRepository: ApodRepository
+    lateinit var sut: UseCaseConfiguration
+
+    @BeforeEach
+    fun setUp() {
+        apodGateway = mockk()
+        apodRepository = mockk()
+        sut = UseCaseConfiguration(
+            apodGateway = apodGateway, apodRepository = apodRepository
+        )
+    }
+
+    @Test
+    fun `on fetchTodayApodUseCase returns FetchTodayApodUseCase`() {
+        val result = sut.fetchTodayApodUseCase()
+
+        assertNotNull(result)
+    }
+}
